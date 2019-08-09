3cc6ec2e95228930471990d29030bb554c6be883
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-09 16:34:26 +0200

    add get/post + insert into db

--- index.php
+++ index.php
@@ -8,10 +8,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "connection succesfull";
+
+    $title = $_GET['title'];
+    $note = $_POST['note'];
+
+    echo $title;
+    echo $note;
+
+    $sql = "INSERT INTO note_db.note_tb (title, note)
+    VALUES ('$title', '$note')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
-
-$_GET['title'];
-
-echo $_GET['title'];
-
-?>
