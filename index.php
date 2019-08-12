67108b7224362d1051c083c7a3f136bf5531fe97
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-12 11:22:59 +0200

    add execution

--- index.php
+++ index.php
@@ -1,55 +1,56 @@
 <?php
 include "keys.php";
+$errors = [];
 
 //  Create connection
 $conn = new mysqli($servername, $username, $password);
 //  Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "connection succesfull";
+    $errors['connection'] = "Connection failed";
 }
 
 //  CREATE SUPERGLOBALS
 $title_input = $_GET['title'];
 $note_input = $_POST['note'];
 
-echo "<br><br> title_input= ".$title_input;
-echo "<br> note_input= ".$note_input;
+try {
 
-// PREPARE
-$stmt = $conn->prepare("INSERT INTO note_db.note_tb (title, note) VALUES (?, ?)");
-$stmt->bind_param("ss", $title, $note);
+    // PREPARE
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//  SANITIIZE
-$clean_title = filter_var($title_input, FILTER_SANITIZE_STRING);
-$clean_note = filter_var($note_input, FILTER_SANITIZE_STRING);
+    $stmt = $conn->prepare("INSERT INTO $tbname (title, note)
+    VALUES (:title, :note)");
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':note', $note);
 
-//  VALIDATE
-echo "<br> clean_title= ".$clean_title;
+    //  SANITIIZE
+    $clean_title = filter_var($title_input, FILTER_SANITIZE_STRING);
+    $clean_note = filter_var($note_input, FILTER_SANITIZE_STRING);
 
-if (empty($clean_title)) {
-    echo "please fill in a title.";
-} else {
-    $validate_title = filter_var($clean_title, FILTER_VALIDATE_REGEXP);
-    if (filter_var($clean_title, FILTER_VALIDATE_REGEXP)) {
-        echo "please enter a title with a valid syntax.";
+    //  VALIDATE
+    if (empty($clean_title)) {
+        $errors['validate_title'] = "please fill in a title.";
     } else {
-        echo "title is validated";
+        $validated_title = $clean_title;
     }
+
+    if (empty($clean_note)) {
+        $errors['validate_note'] = "Your note is empty.";
+    } else {
+        $validated_note = $clean_note;
+    }
+
+    //  EXECUTE
+    $title = $validated_title;
+    $note = $validated_note;
+    $stmt->execute();
+
+    echo "New records created successfully";
 }
-
-// if (empty($clean_note)) {
-//     echo "please fill in a note.";
-// } else if (!filter_var($clean_note, FILTER_VALIDATE_REGEXP)) {
-//     echo "please enter a note with a valid syntax";
-// } else {
-//     echo "note is validated.";
-// }
-
-// //  EXECUTE
-// if ($conn->query($stmt) === TRUE) {
-//     echo "New record created successfully";
-// } else {
-//     echo "Error: " . $stmt . "<br>" . $conn->error;
-// }
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+    
+$conn = null;
